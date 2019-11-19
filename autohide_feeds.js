fa58767507a441259d7942f7800c094b9636a709
David <32897194+gloony@users.noreply.github.com>
2019-11-19 03:34:09 +0100

    Create autohide_feeds.js

---
+++ autohide_feeds.js
@@ -0,0 +1,7 @@
+require(['dojo/_base/kernel', 'dojo/ready'], function (dojo, ready) {
+	ready(function () {
+		PluginHost.register(PluginHost.HOOK_INIT_COMPLETE, () => {
+			Feeds.toggle();
+		});
+	});
+});
